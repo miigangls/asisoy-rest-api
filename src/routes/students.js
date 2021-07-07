00fb535c2d4c697bb09bc736de6c50e6d1f49f9e
miigangls <miigangls@gmail.com>
2021-07-06 19:59:35 -0500

    new retunt result server.

--- src/routes/students.js
+++ src/routes/students.js
@@ -1,6 +1,6 @@
 const express = require('express')
-, router = express.Router()
-, mysqlConnection = require('../database')
+    , router = express.Router()
+    , mysqlConnection = require('../database')
 
 router.get('/allStudents', async (req, res) => {
     await mysqlConnection.query('SELECT * FROM alumno', (err, rows, fields) => {
@@ -14,43 +14,40 @@ router.get('/allStudents', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     let { CODALUM, ID_ALUMNO } = req.body
-    console.log(req.body)
     await mysqlConnection.query(`SELECT * FROM alumno AS al JOIN brectemp AS br on al.CODALUM = br.CODALUM WHERE al.CODALUM = ${CODALUM} AND al.ID_ALUMNO = ${ID_ALUMNO} LIMIT 1`, (err, rows, fields) => {
         if (!err) {
-            if(rows.length) {
-                res.json({
-                    status: true,
-                    data: rows
-                })
+            if (rows.length) {
+                res.status(200).send({ status: 200, data: rows });
             } else {
-                res.json({
-                    status: false
-                })
+                res.status(500).send({ status: 200, data: [] });
             }
         } else {
-            console.log(err);
+            res.status(500).send({ status: 500, error: "Internal Server Error", message: err });
         }
     })
 })
+
 //1203563381
-router.get('/subjectByStudent', async (req, res) => {
-    await mysqlConnection.query(`select a.AREAS_ASIG, a.COD, a.orden as asignaturas, dg.ASIGNATURA as docente_grupo, d.NOMBRES, 
-        d.APELLIDOS, d.NUM_DOC FROM asignaturas AS a, docente_grupo AS dg, docentes AS d WHERE dg.GRUPO = ${req.body.curso} AND dg.DOCENTE = d.NUM_DOC 
-        AND dg.ASIGNATURA=a.COD and a.COD != 98;`, (err, rows, fields) => {
+router.post('/subjectByStudent', async (req, res) => {
+    const { CURSO } = req.body
+    await mysqlConnection.query(`SELECT a.AREAS_ASIG, a.COD, a.orden AS asignaturas, dg.ASIGNATURA AS docente_grupo, d.NOMBRES, 
+        d.APELLIDOS, d.NUM_DOC FROM asignaturas AS a, docente_grupo AS dg, docentes AS d WHERE dg.GRUPO = ? AND dg.DOCENTE = d.NUM_DOC 
+        AND dg.ASIGNATURA=a.COD and a.COD != 98;`, [CURSO], (err, rows, fields) => {
         if (!err) {
-            res.json(rows)
+            res.status(200).send({ status: 200, message: "", data: rows });
         } else {
-            console.log(err);
+            res.status(500).send({ status: 500, error: "Internal Server Error", message: err, data: [] });
         }
     })
 })
 
 router.post('/studentsBysubject', async (req, res) => {
-    let { ASIGNATURA } = req.body
-    await mysqlConnection.query(`select * from alumno inner join brectemp on alumno.CODALUM = brectemp.CODALUM where brectemp.ASIGNATURA = ?`, [ASIGNATURA], (err, rows, fields) => {
+    const { ASIGNATURA } = req.body
+    await mysqlConnection.query(`SELECT * FROM alumno INNER JOIN brectemp ON alumno.CODALUM = brectemp.CODALUM WHERE brectemp.ASIGNATURA = ?`, [ASIGNATURA], (err, rows, fields) => {
         if (!err) {
-            res.json(rows)
+            res.status(200).send({ status: 200, message: "", data: rows });
         } else {
+            res.status(500).send({ status: 500, error: "Internal Server Error", message: err, data: [] });
             console.log(err);
         }
     })
@@ -58,12 +55,12 @@ router.post('/studentsBysubject', async (req, res) => {
 
 
 router.post('/questions', async (req, res) => {
-    let { CODTEEVALUACION } = req.body
+    const { CODTEEVALUACION } = req.body
     await mysqlConnection.query(`SELECT * FROM Criterios WHERE codtevaluacion = ?  AND activo = ? ;`, [CODTEEVALUACION, 1], (err, rows, fields) => {
         if (!err) {
-            res.status(200).send({status:200,  message: "", data: rows});
+            res.status(200).send({ status: 200, message: "", data: rows });
         } else {
-            res.status(500).send({status:500, error:"Internal Server Error", message: err, data: []});
+            res.status(500).send({ status: 500, error: "Internal Server Error", message: err, data: [] });
             console.log(err);
         }
     })
