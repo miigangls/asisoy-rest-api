dbb48a7b15f8749988ddc5f22a75be717de57b77
miigangls <miigangls@gmail.com>
2021-07-06 19:58:48 -0500

    app index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-const express = require('express')
-, app = express()
-, bodyParser = require('body-parser')
-// Settings
-app.set('port', process.env.PORT || 3000)
+require('dotenv').config();
+const app = require('./app')
 
-// Middlewares
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.get('*', function (req, res) {
+    res.send('welcom rest API')
+})
 
 // Routes
 app.use(require('./routes/students'))
